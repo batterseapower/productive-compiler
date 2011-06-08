d803ab552915aab7ed771311f1046fa7354117c1
Max Bolingbroke <batterseapower@hotmail.com>
2011-06-08 15:52:06 +0100

    Implement lambdas (maybe)

--- Main.hs
+++ Main.hs
@@ -2,11 +2,13 @@
 module Main (main) where
 
 import Control.Monad
+import Control.Monad.IO.Class
 
 import Data.Char
 import Data.Int
 import Data.Word
 import Data.List
+import Data.IORef
 
 import Foreign.Ptr
 
@@ -46,24 +48,39 @@ test_term = PrimOp Add [Value (Literal 1), Value (Literal 2)]
 main :: IO ()
 main = do
     initializeNativeTarget
-    f <- simpleFunction $ unCM (compileTop test_term) (CE { symbolValues = M.empty })
+    f <- simpleFunction $ fmap snd $ runCompileM (compileTop test_term) (CE { symbolValues = M.empty }) ["float" ++ show i | i <- [1..]]
     f
 
 data CompileEnv = CE {
-    symbolValues    :: M.Map Var (CodeGenFunction (Ptr ()) (Value (Ptr ())))
+    symbolValues :: M.Map Var (CodeGenFunction (Ptr ()) (Value (Ptr ())))
   }
 
-newtype CompileM a = CM { unCM :: CompileEnv -> CodeGenModule a }
+data CompileState = CS {
+    pendingFunctions :: [CompileState -> CodeGenModule CompileState],
+    functionNameSupply :: [String]
+  }
+
+newtype CompileM a = CM { unCM :: CompileEnv -> CompileState -> CodeGenModule (CompileState, a) }
+
+runCompileM :: CompileM a -> CompileEnv -> [String] -> CodeGenModule ([String], a)
+runCompileM cm env names = do
+    (s, x) <- unCM cm env (CS { pendingFunctions = [], functionNameSupply = names })
+    names <- discharge_pending s
+    return (names, x)
+  where
+    discharge_pending s = case pendingFunctions s of
+      []       -> return (functionNameSupply s)
+      (cf:cfs) -> cf (s { pendingFunctions = cfs }) >>= discharge_pending
 
 instance Functor CompileM where
     fmap = liftM
 
 instance Monad CompileM where
-    return x = CM $ \_ -> return x
-    mx >>= fxmy = CM $ \e -> unCM mx e >>= \x -> unCM (fxmy x) e
+    return x = CM $ \_ s -> return (s, x)
+    mx >>= fxmy = CM $ \e s -> unCM mx e s >>= \(s, x) -> unCM (fxmy x) e s
 
 liftCGM :: CodeGenModule a -> CompileM a
-liftCGM cgm = CM $ \_ -> cgm
+liftCGM cgm = CM $ \_ s -> fmap ((,) s) cgm
 
 
 compileTop :: Term -> CompileM (Function (IO ()))
@@ -77,22 +94,50 @@ compileTop e = do
         ret ()
 
 compile :: Term -> CompileM (Function (IO (Ptr ())))
-compile e = CM $ \env -> createFunction ExternalLinkage $ compile' env e ret
+compile e = CM $ \env s -> tunnelIO (createFunction ExternalLinkage) $ compile' env s e (\s value_ptr -> fmap ((,) s) (ret value_ptr))
+
+tunnelIO :: (MonadIO m, MonadIO n)
+         => (m a -> n b)
+         -> m (c, a)
+         -> n (c, b)
+tunnelIO control arg = do
+    -- Urgh, have to resort to tunneling through IORef to get the new State out
+    s_ref <- liftIO $ newIORef (error "tunnelIO: unfilled IORef")
+    fun <- control $ do
+        (s, res) <- arg
+        liftIO $ writeIORef s_ref s
+        return res
+    s <- liftIO $ readIORef s_ref
+    return (s, fun)
+
+tunnelIO2 :: (MonadIO m, MonadIO n)
+          => ((arg1 -> arg2 -> m a) -> n b)
+          -> (arg1 -> arg2 -> m (c, a))
+          -> n (c, b)
+tunnelIO2 control arg = do
+    -- Urgh, have to resort to tunneling through IORef to get the new State out
+    s_ref <- liftIO $ newIORef (error "tunnelIO: unfilled IORef")
+    fun <- control $ \arg1 arg2 -> do
+        (s, res) <- arg arg1 arg2
+        liftIO $ writeIORef s_ref s
+        return res
+    s <- liftIO $ readIORef s_ref
+    return (s, fun)
 
 -- NB: this code assumes Int32s fit into pointers! Probably safe, but...
-compile' :: CompileEnv -> Term
-         -> (Value (Ptr ()) -> CodeGenFunction (Ptr ()) r)
+compile' :: CompileEnv -> CompileState -> Term
+         -> (CompileState -> Value (Ptr ()) -> CodeGenFunction (Ptr ()) r)
          -> CodeGenFunction (Ptr ()) r
-compile' env (Var x) k = compileVar env x >>= k
-compile' env (Value v) k = compileValue env v >>= k
-compile' env (App e x) k = compile' env e $ \closure_ptr -> do
+compile' env s (Var x) k = compileVar env x >>= k s
+compile' env s (Value v) k = compileValue env s v >>= uncurry k
+compile' env s (App e x) k = compile' env s e $ \s closure_ptr -> do
     arg_ptr <- compileVar env x
     
-    fun_ptr_ptr <- bitcast closure_ptr :: CodeGenFunction (Ptr ()) (Value (Ptr (Ptr (Ptr () -> Ptr () -> IO (Ptr ())))))
+    fun_ptr_ptr <- bitcast closure_ptr :: CodeGenFunction (Ptr ()) (Function (Ptr (Ptr () -> Ptr () -> IO (Ptr ()))))
     fun_ptr <- load fun_ptr_ptr
     
-    call fun_ptr closure_ptr arg_ptr >>= k
-compile' env (Case e alts) k = compile' env e $ \data_ptr -> do
+    call fun_ptr closure_ptr arg_ptr >>= k s
+compile' env s (Case e alts) k = compile' env s e $ \s data_ptr -> do
     -- Retrieve the tag:
     tag_ptr <- bitcast data_ptr :: CodeGenFunction (Ptr ()) (Value (Ptr Int32))
     tag <- load tag_ptr
@@ -102,59 +147,59 @@ compile' env (Case e alts) k = compile' env e $ \data_ptr -> do
     panic_block <- newBasicBlock
     (blocks, define_blocks) <- fmap unzip $ forM alts $ \(dc, xs, e) -> do
         alt_block <- newBasicBlock
-        let define_block = do
+        let define_block s = do
               defineBasicBlock alt_block
               field_base_ptr <- bitcast data_ptr :: CodeGenFunction (Ptr ()) (Value (Ptr (Ptr ())))
-              env <- foldM (\env (x, offset) -> do {
-                    field_ptr <- getElementPtr field_base_ptr (offset :: Int32, ());
-                    value_ptr <- load field_ptr;
-                    return (env { symbolValues = M.insert x (return value_ptr) (symbolValues env) })
-                  }) env (xs `zip` [1..])
-              result_ptr <- compile' env e $ \result_ptr -> br join_block >> return result_ptr
-              return (result_ptr, alt_block)
+              env <- forAccM_ env (xs `zip` [1..]) $ \env (x, offset) -> do
+                  field_ptr <- getElementPtr field_base_ptr (offset :: Int32, ())
+                  value_ptr <- load field_ptr
+                  return (env { symbolValues = M.insert x (return value_ptr) (symbolValues env) })
+              (s, result_ptr) <- compile' env s e $ \s result_ptr -> br join_block >> return (s, result_ptr)
+              return (s, (result_ptr, alt_block))
         return ((constOf (dataConTag dc), alt_block), define_block)
     
     -- Jump into appropriate tail code:
     switch tag panic_block blocks
     
     -- Define tail code
-    phi_data <- sequence define_blocks
+    (s, phi_data) <- accumLM s define_blocks
     defineBasicBlock panic_block
     unreachable
     defineBasicBlock join_block
-    phi phi_data >>= k
-compile' env (Let x e_bound e_body) k = compile' env e_bound $ \bound_ptr -> do
-    compile' (env { symbolValues = M.insert x (return bound_ptr) (symbolValues env) }) e_bound k
-compile' env (LetRec xvs e_body) k = do
+    phi phi_data >>= k s
+compile' env s (Let x e_bound e_body) k = compile' env s e_bound $ \s bound_ptr -> do
+    compile' (env { symbolValues = M.insert x (return bound_ptr) (symbolValues env) }) s e_bound k
+compile' env s (LetRec xvs e_body) k = do
     -- FIXME: this does not work properly
     (env, xvs) <- mapAccumLM (\env (x, v) -> do {
                 delay_cell_ptr <- alloca :: CodeGenFunction (Ptr ()) (Value (Ptr (Ptr ())));
                 return (env { symbolValues = M.insert x (load delay_cell_ptr) (symbolValues env) }, (delay_cell_ptr, v))
               }) env xvs
     
-    forM_ xvs $ \(delay_cell_ptr, v) -> do
-        value_ptr <- compileValue env v
+    s <- forAccM_ s xvs $ \s (delay_cell_ptr, v) -> do
+        (s, value_ptr) <- compileValue env s v
         store value_ptr delay_cell_ptr
+        return s
     
-    compile' env e_body k
-compile' env (PrimOp pop es) k = cpsBindN [TH (\(k :: Value Int32 -> m r) -> compile' env e (\value_ptr -> bitcast value_ptr >>= k)) | e <- es] $ \arg_values -> do
-    res_value <- case (pop, arg_values) of
+    compile' env s e_body k
+compile' env s (PrimOp pop es) k = cpsBindN [TH (\s (k :: CompileState -> Value Int32 -> m r) -> compile' env s e (\s value_ptr -> bitcast value_ptr >>= k s)) | e <- es] s $ \s arg_ints -> do
+    res_int <- case (pop, arg_ints) of
         (Add,      [i1, i2]) -> add i1 i2
         (Subtract, [i1, i2]) -> sub i1 i2
         (Multiply, [i1, i2]) -> mul i1 i2
         _ -> error "Bad primitive operation arity"
-    bitcast res_value >>= k
-compile' env (Weaken x e) k = compile' (env { symbolValues = M.delete x (symbolValues env) }) e k
-compile' env (Delay e) k = error "TODO: unimplemented"
+    bitcast res_int >>= k s
+compile' env s (Weaken x e) k = compile' (env { symbolValues = M.delete x (symbolValues env) }) s e k
+compile' env s (Delay e) k = error "TODO: unimplemented"
 
 compileVar :: CompileEnv -> Var -> CodeGenFunction (Ptr ()) (Value (Ptr ()))
 compileVar env x = case M.lookup x (symbolValues env) of
-    Nothing        -> error $ "Unbound variable " ++ show x
-    Just get_value -> get_value
+    Nothing            -> error $ "Unbound variable " ++ show x
+    Just get_value_ptr -> get_value_ptr
 
-compileValue :: CompileEnv -> Val -> CodeGenFunction (Ptr ()) (Value (Ptr ()))
-compileValue env v = case v of
-    Literal l  -> bitcast (valueOf l)
+compileValue :: CompileEnv -> CompileState -> Val -> CodeGenFunction (Ptr ()) (CompileState, Value (Ptr ()))
+compileValue env s v = case v of
+    Literal l  -> fmap ((,) s) $ bitcast (valueOf l)
     Data dc xs -> do
         -- Allocate space for one pointer per data item, and one pointer for the tag
         data_ptr <- arrayMalloc (1 + genericLength xs :: Word32) :: CodeGenFunction (Ptr ()) (Value (Ptr (Ptr ())))
@@ -169,8 +214,36 @@ compileValue env v = case v of
             value_ptr <- compileVar env x
             store value_ptr field_ptr
     
-        bitcast data_ptr
-    Lambda x e -> error "TODO: unimplemented"
+        fmap ((,) s) $ bitcast data_ptr
+    Lambda x e -> do
+        let (name:names) = functionNameSupply s
+            (get_in_closure_values, get_out_closure_values) = unzip $ flip map ([1..] `zip` M.toList (symbolValues env)) $
+                                                        \(offset, (x, get_value_ptr)) -> let reload_value_ptr closure_ptr = do
+                                                                                                field_ptr <- getElementPtr closure_ptr (offset :: Int32, ())
+                                                                                                load field_ptr
+                                                                                         in ((offset, get_value_ptr), (x, reload_value_ptr))
+        
+        -- Allocate space for one pointer per closure variable, and one pointer for the code
+        closure_ptr <- arrayMalloc (1 + genericLength get_in_closure_values :: Word32) :: CodeGenFunction (Ptr ()) (Value (Ptr (Ptr ())))
+        
+        -- Poke in the code
+        fun_ptr_ptr <- bitcast closure_ptr :: CodeGenFunction (Ptr ()) (Function (Ptr (Ptr () -> Ptr () -> IO (Ptr ()))))
+        fun_ptr <- externFunction name
+        store fun_ptr fun_ptr_ptr
+        
+        -- Poke in the closure variables
+        forM get_in_closure_values $ \(offset, get_value_ptr) -> do
+            field_ptr <- getElementPtr closure_ptr (offset, ())
+            value_ptr <- get_value_ptr
+            store value_ptr field_ptr
+        
+        -- Pend the definition of the code for function referened by that closure
+        let define_function s = fmap (\(s, fun_ptr :: Function (Ptr (Ptr ()) -> Ptr () -> IO (Ptr ()))) -> s) $ tunnelIO2 (createNamedFunction ExternalLinkage name) $ \closure_ptr arg_ptr -> do
+                closed_value_ptrs <- forM get_out_closure_values $ \(x, reload_value_ptr) -> do
+                    value_ptr <- reload_value_ptr closure_ptr
+                    return (x, return value_ptr)
+                compile' (env { symbolValues = M.insert x (return arg_ptr) (M.fromList closed_value_ptrs) }) s e $ \s value_ptr -> fmap ((,) s) (ret value_ptr)
+        fmap ((,) (s { functionNameSupply = names, pendingFunctions = define_function : pendingFunctions s })) $ bitcast closure_ptr
 
 
 mapAccumLM :: (Monad m) => (acc -> x -> m (acc, y)) -> acc -> [x] -> m (acc, [y])
@@ -180,12 +253,26 @@ mapAccumLM f s (x:xs) = do
      (s'',ys) <- mapAccumLM f s' xs
      return (s'',y:ys)
 
+accumLM :: (Monad m) => acc -> [acc -> m (acc, y)] -> m (acc, [y])
+accumLM s []     = return (s, [])
+accumLM s (f:fs) = do
+     (s', y)  <- f s
+     (s'',ys) <- accumLM s' fs
+     return (s'',y:ys)
+
 -- Impredicative types do not seem to work too well (I can't get the call site to type-check),
 -- so I'm working around the issue with this small wrapper:
-newtype TypeHack a m = TH { unTH :: forall r. (a -> m r) -> m r }
+newtype TypeHack s a m = TH { unTH :: forall r. s -> (s -> a -> m r) -> m r }
 
-cpsBindN :: [TypeHack a m]
-         -> ([a] -> m r)
+cpsBindN :: [TypeHack s a m]
+         -> s
+         -> (s -> [a] -> m r)
          -> m r
-cpsBindN []     k = k []
-cpsBindN (r:rs) k = unTH r $ \x -> cpsBindN rs $ \xs -> k (x:xs)
+cpsBindN []     s k = k s []
+cpsBindN (r:rs) s k = unTH r s $ \s x -> cpsBindN rs s $ \s xs -> k s (x:xs)
+
+forAccM_ :: Monad m => acc -> [a] -> (acc -> a -> m acc) -> m acc
+forAccM_ acc []     _ = return acc
+forAccM_ acc (x:xs) f = do
+    acc <- f acc x
+    forAccM_ acc xs f
